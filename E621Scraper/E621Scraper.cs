f87283c8d84d9b834b1e5fd53cc1ccd7a51bdede
Zylanx <zylanxdragon@gmail.com>
2021-12-24 16:14:07 +1100

    Rename main class

---
+++ E621Scraper/E621Scraper.cs
@@ -0,0 +1,13 @@
+﻿// This is a janky test, not intended to work well or be well documented.
+// Don't expect quality code.
+
+namespace E621Scraper
+{
+    public class E621Scraper
+    {
+        public void Main()
+        {
+        
+        }
+    }
+}
