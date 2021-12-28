bfb579392515a83037656ebd373497dc3202e7c6
Matthew <mrdunn@gmail.com>
2021-12-28 14:03:54 +1030

    Added null to strings

--- E621Scraper/ApiConfig.cs
+++ E621Scraper/ApiConfig.cs
@@ -8,7 +8,7 @@ namespace E621Scraper
 {
     public class ApiConfig
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
     }
 }
