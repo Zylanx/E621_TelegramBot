91772178794b2c9c2bf7bff9f08b3c5b84e48087
Zylanx <zylanxdragon@gmail.com>
2022-01-01 21:05:12 +1100

    Add basic list functionality

--- E621TelegramBot/Commands/Subscription/ListSubscriptions.cs
+++ E621TelegramBot/Commands/Subscription/ListSubscriptions.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using E621Shared;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -7,15 +8,29 @@ namespace E621TelegramBot.Commands.Subscription
 {
     public class ListSubscriptions : BaseBotCommand
     {
-        public ListSubscriptions()
+        private readonly SubscriberRepo _subRepo;
+
+        public ListSubscriptions(SubscriberRepo subRepo)
         {
             Command = "list";
             Description = "List all your subscriptions";
+
+            _subRepo = subRepo;
         }
 
-        public override Task Execute(ITelegramBotClient botClient, Update update)
+        public override async Task Execute(ITelegramBotClient botClient, Update update)
         {
-            throw new NotImplementedException();
+            var subList = await _subRepo.ListSubscriptionsForTelegramUser(update.Message!.From!.Id);
+            string response = string.Join("\n", subList.Select((subscription, i) => $"{i + 1}. {subscription.Tag}"));
+
+            if (string.IsNullOrWhiteSpace(response))
+            {
+                response = "No subscriptions to list.";
+            }
+
+            await botClient.SendTextMessageAsync(
+                update.Message!.Chat.Id,
+                response);
         }
     }
 }
